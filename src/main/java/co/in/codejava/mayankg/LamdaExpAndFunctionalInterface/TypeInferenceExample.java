4df8df7a3aa5a64e02a1a8343bbf8b50976c7357
Mayank Gupta <ce.mayank8@gmail.com>
2023-06-21 22:13:55 +0530

    TypeInference example

--- src/main/java/co/in/codejava/mayankg/LamdaExpAndFunctionalInterface/TypeInferenceExample.java
+++ src/main/java/co/in/codejava/mayankg/LamdaExpAndFunctionalInterface/TypeInferenceExample.java
@@ -3,15 +3,17 @@ package co.in.codejava.mayankg.LamdaExpAndFunctionalInterface;
 public class TypeInferenceExample {
     public static void main(String[] args) {
         String str = "findLengthOfString";
-        StringLengthLambda myLambda = s -> s.length();
-        printStringLengthUsingLambda(myLambda);
+        StringLengthLambda myLambda = (String s) -> s.length();
+        printStringLengthUsingLambda(str, myLambda);
     }
 
-    private static void printStringLengthUsingLambda(StringLengthLambda l) {
-        l.getLength()
+    private static void printStringLengthUsingLambda(String s, StringLengthLambda lambda) {
+        System.out.println(lambda.getLength(s));
     }
 
+    @FunctionalInterface
     interface StringLengthLambda {
+        //this interface should have only one declared method
         int getLength(String s);
     }
 }
