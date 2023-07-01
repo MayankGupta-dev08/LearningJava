fdc3ab958cc942c7cd074a90770adf9c0830a4aa
techno-Sorcerer <techno.sorcerer3@gmail.com>
2023-07-01 16:29:39 +0530

    functional interface basics

--- src/main/java/co/in/codejava/mayankg/LamdaExpAndFunctionalInterface/Greeting.java
+++ src/main/java/co/in/codejava/mayankg/LamdaExpAndFunctionalInterface/Greeting.java
@@ -1,5 +1,18 @@
 package co.in.codejava.mayankg.LamdaExpAndFunctionalInterface;
 
+/**
+ * Functional Interfaces:
+ * - We can have as many methods as we want in an interface but since we are making it a functional interface
+ * we can have only one declared method and
+ * rest methods could be defined methods (with default implementation which is supported after Java8)
+ */
+
+@FunctionalInterface
 public interface Greeting {
     public void perform();
+
+    // since this method has default implementation --> no need to override this in class which is implementing this interface.
+    public static void anotherMethod() {
+        System.out.println("another method");
+    }
 }
