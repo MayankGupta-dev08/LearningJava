be15ec10b1a0631557815c124586e4a1cb345a70
techno-Sorcerer <techno.sorcerer3@gmail.com>
2023-07-02 18:19:37 +0530

    Exc1 solution2 using java8 lambda exp

--- src/main/java/co/in/codejava/mayankg/Excercise1/Exc1SolutionJava8.java
+++ src/main/java/co/in/codejava/mayankg/Excercise1/Exc1SolutionJava8.java
@@ -1,20 +1,31 @@
 package co.in.codejava.mayankg.Excercise1;
 
+import java.nio.charset.UnsupportedCharsetException;
 import java.util.Comparator;
 import java.util.List;
 
 public class Exc1SolutionJava8 {
 
     // Q1. Sort the list by lastName & position
-    void sortPlayers(List<Footballers> players, PlayerAttributes playerAttribute) {
-
+    void sortPlayers(List<Footballers> players, String playerAttribute) {
+        if (PlayerAttributes.FIRST_NAME.name().equalsIgnoreCase(playerAttribute)) {
+            players.sort((Footballers p1, Footballers p2) -> {
+                return p1.getFirstName().compareTo(p2.getFirstName());
+            });
+        } else if (PlayerAttributes.LAST_NAME.name().equalsIgnoreCase(playerAttribute)) {
+            players.sort((p1, p2) -> p1.getLastName().compareTo(p2.getLastName()));
+        } else if (PlayerAttributes.JERSEY_NUMBER.name().equalsIgnoreCase(playerAttribute)) {
+            players.sort(Comparator.comparing(Footballers::getJerseyNum));
+        } else if (PlayerAttributes.POSITION.name().equalsIgnoreCase(playerAttribute)) {
+            players.sort((p1, p2) -> p1.getPosition().compareTo(p2.getPosition()));
+        } else {
+            System.out.println("Invalid player attribute: " + " used, operation failed!!");
+        }
     }
 
     // Q2. print all elements in the list
     void printAll(List<Footballers> players) {
-        for (Footballers player : players) {
-            System.out.println(player);
-        }
+        printConditionally(players, player -> true);
     }
 
     // Q3. print all elements on basis of Condition
@@ -26,11 +37,27 @@ public class Exc1SolutionJava8 {
         }
     }
 
-    protected void printOnBasisOfPosition(List<Footballers> players, String position) {
+    void printOnBasisOfPosition(List<Footballers> players, String position) {
+        if (!List.of("ST", "MF", "DF", "FW", "GK").contains(position))
+            throw new UnsupportedCharsetException("Invalid position entered, operation failed!!");
 
+        System.out.println("Players for " + position + " position:");
+        printConditionally(players, player -> player.getPosition().equalsIgnoreCase(position));
     }
 
-    protected void printIfFirstNameStartsWithLetter(List<Footballers> players, Character letter) {
+    void printIfFirstNameStartsWithLetter(List<Footballers> players, Character letter) {
+        if (!Character.isAlphabetic(letter))
+            throw new UnsupportedCharsetException("Invalid character entered, operation failed!!");
 
+        System.out.println("Players with first name starting with " + letter + ":");
+        printConditionally(players, player -> player.getFirstName().toLowerCase().startsWith(String.valueOf(letter).toLowerCase()));
+    }
+
+    void printIfLastNameStartsWithLetter(List<Footballers> players, Character letter) {
+        if (!Character.isAlphabetic(letter))
+            throw new UnsupportedCharsetException("Invalid character entered, operation failed!!");
+
+        System.out.println("Players with last name starting with " + letter + ":");
+        printConditionally(players, player -> player.getLastName().toLowerCase().startsWith(String.valueOf(letter).toLowerCase()));
     }
 }
