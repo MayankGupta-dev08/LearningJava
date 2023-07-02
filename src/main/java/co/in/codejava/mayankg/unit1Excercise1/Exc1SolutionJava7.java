e1a9dd408d15ee16b28c638690fbc5e44862fa5d
techno-Sorcerer <techno.sorcerer3@gmail.com>
2023-07-03 02:58:32 +0530

    Understanding the different functional interfaces available from java8

--- src/main/java/co/in/codejava/mayankg/unit1Excercise1/Exc1SolutionJava7.java
+++ src/main/java/co/in/codejava/mayankg/unit1Excercise1/Exc1SolutionJava7.java
@@ -5,7 +5,6 @@ import co.in.codejava.mayankg.common.PlayerAttributes;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Predicate;
 
 public class Exc1SolutionJava7 {
 
