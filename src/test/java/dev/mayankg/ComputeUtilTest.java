c13b61d02b3946a59dc8f7ff26ebaec9a73cee4c
Mayank Gupta <ce.mayank8@gmail.com>
2024-01-05 01:40:03 +0530

    learning JUnit5..

---
+++ src/test/java/dev/mayankg/ComputeUtilTest.java
@@ -0,0 +1,23 @@
+package dev.mayankg;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComputeUtilTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void sum() {
+        fail("Not yet implemented");
+    }
+}
