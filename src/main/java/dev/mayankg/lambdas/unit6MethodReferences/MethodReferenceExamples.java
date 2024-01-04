ed14794f409caae71af95d20c2a844bf0cbe8bd0
Mayank Gupta <ce.mayank8@gmail.com>
2024-01-05 00:10:06 +0530

    method reference in lambdas (java8)

--- src/main/java/dev/mayankg/lambdas/unit6MethodReferences/MethodReferenceExamples.java
+++ src/main/java/dev/mayankg/lambdas/unit6MethodReferences/MethodReferenceExamples.java
@@ -1,12 +1,25 @@
 package dev.mayankg.lambdas.unit6MethodReferences;
 
+import java.util.function.BiFunction;
+
 public class MethodReferenceExamples {
     public static void main(String[] args) {
         Thread t1 = new Thread(MethodReferenceExamples::printMessage); // () -> method() === ClassName::methodName
         t1.start();
+
+        int sum = compute(10, 20, MethodReferenceExamples::printSum); // (a, b) -> printSum(a, b)
+        System.out.println(sum);
     }
 
-    public static void printMessage() {
+    static void printMessage() {
         System.out.println("Hello");
+    }
+
+    static int printSum(int a, int b) {
+        return a + b;
+    }
+
+    static int compute(int a, int b, BiFunction<Integer, Integer, Integer> biFunction) {
+        return biFunction.apply(a, b);
     }
 }
