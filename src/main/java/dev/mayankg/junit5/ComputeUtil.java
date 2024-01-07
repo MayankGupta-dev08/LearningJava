afea32be559a42c766d29b7bb8f4ef57e886e13f
Mayank Gupta <ce.mayank8@gmail.com>
2024-01-07 20:55:18 +0530

    learning JUnit5..

--- src/main/java/dev/mayankg/junit5/ComputeUtil.java
+++ src/main/java/dev/mayankg/junit5/ComputeUtil.java
@@ -1,8 +1,21 @@
 package dev.mayankg.junit5;
 
 public class ComputeUtil {
+
     public static int sum(int a, int b) {
         return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
     }
 
     public static double computeCircleArea(double radius) {
--- src/test/java/dev/mayankg/ComputeUtilTest.java
+++ src/test/java/dev/mayankg/ComputeUtilTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ComputeUtilTest {
 
@@ -19,7 +19,8 @@ class ComputeUtilTest {
     void tearDown() {
     }
 
-    @Test @DisplayName("sum of two numbers")
+    @Test
+    @DisplayName("sum of two numbers")
     void testSum() {
         // fail("Not yet implemented");
         int expected = 3;
@@ -27,9 +28,16 @@ class ComputeUtilTest {
         assertEquals(expected, actual, "The method should add two numbers");
     }
 
-    @Test @DisplayName("area of a circle")
-    void testAreaOfCircle(){
+    @Test
+    @DisplayName("area of a circle")
+    void testAreaOfCircle() {
         double expected = 314.1592653589793;
         assertEquals(expected, ComputeUtil.computeCircleArea(10), "should return area of a circle");
+    }
+
+    @Test
+    @DisplayName("divide by 0 should throw arithmetic exception")
+    void testDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> ComputeUtil.divide(10, 0));
     }
 }
