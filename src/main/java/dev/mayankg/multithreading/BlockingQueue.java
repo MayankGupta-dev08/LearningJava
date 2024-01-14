84b51bb6c493cbe8b5da9bcdc04547c81f64ea5d
Mayank Gupta <ce.mayank8@gmail.com>
2024-01-14 18:36:24 +0530

    Producer-Consumer problem with wait() and notifyAll() method usage

---
+++ src/main/java/dev/mayankg/multithreading/BlockingQueue.java
@@ -0,0 +1,43 @@
+package dev.mayankg.multithreading;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Producer-Consumer problem
+ */
+public class BlockingQueue {
+    private Queue<Integer> queue;
+    private int capacity;
+
+    public BlockingQueue(int capacity) {
+        this.capacity = capacity;
+        this.queue = new LinkedList<>();
+    }
+
+    public boolean add(int item) {
+        synchronized (queue) {
+            while (queue.size() == capacity) try {
+                queue.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            queue.add(item);
+            queue.notifyAll();  //notify all the threads which are in waiting state (due to remove)
+            return true;
+        }
+    }
+
+    public int remove() {
+        synchronized (queue) {
+            while (queue.isEmpty()) try {
+                queue.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Integer elem = queue.poll();
+            notifyAll();
+            return elem;
+        }
+    }
+}
