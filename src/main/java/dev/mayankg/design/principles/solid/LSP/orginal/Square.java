39c66057f45cf90c8ad0ae2121d0fa02e60c4a13
Mayank Gupta <ce.mayank8@gmail.com>
2024-02-10 20:33:04 +0530

    Design Principles & Patterns: LSP#problem

---
+++ src/main/java/dev/mayankg/design/principles/solid/LSP/orginal/Square.java
@@ -0,0 +1,22 @@
+package dev.mayankg.design.principles.solid.LSP.orginal;
+
+class Square extends Rectangle {
+    public Square(int side) {
+        super(side, side);
+    }
+
+    @Override
+    public void setWidth(int width) {
+        setSide(width);
+    }
+
+    @Override
+    public void setHeight(int height) {
+        setSide(height);
+    }
+
+    public void setSide(int side) {
+        super.setWidth(side);
+        super.setHeight(side);
+    }
+}
