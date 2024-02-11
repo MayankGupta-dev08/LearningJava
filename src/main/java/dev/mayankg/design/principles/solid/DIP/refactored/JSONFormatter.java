41e5f198cfb6b7a827de8b9c499602d4bd6fd98a
Mayank Gupta <ce.mayank8@gmail.com>
2024-02-11 21:20:52 +0530

    Added different build profiles in the pom

---
+++ src/main/java/dev/mayankg/design/principles/solid/DIP/refactored/JSONFormatter.java
@@ -0,0 +1,18 @@
+package dev.mayankg.design.principles.solid.DIP.orginal;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+//Formats message to JSON format
+class JSONFormatter implements Formatter {
+
+    public String format(Message message) throws FormatException {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(message);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new FormatException(e);
+        }
+    }
+}
---
+++ src/main/java/dev/mayankg/design/principles/solid/DIP/refactored/MessagePrinter.java
@@ -0,0 +1,17 @@
+package dev.mayankg.design.principles.solid.DIP.orginal;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+class MessagePrinter {
+
+    //Writes message to a file
+    public void writeMessage(Message msg, String fileName) throws IOException {
+        Formatter formatter = new JSONFormatter();//creates formatter
+        try (PrintWriter writer = new PrintWriter(new FileWriter(fileName))) { //creates print writer
+            writer.println(formatter.format(msg)); //formats and writes message
+            writer.flush();
+        }
+    }
+}
