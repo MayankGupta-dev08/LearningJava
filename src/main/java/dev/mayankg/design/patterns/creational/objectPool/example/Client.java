610f54f25424088239013e9c0a8a66ebd9e531bc
Mayank Gupta <ce.mayank8@gmail.com>
2024-02-20 09:31:43 +0530

    Design Principles & Patterns: Creational # ObjectPoolPattern - example

--- src/main/java/dev/mayankg/design/patterns/creational/objectPool/example/Client.java
+++ src/main/java/dev/mayankg/design/patterns/creational/objectPool/example/Client.java
@@ -1,10 +1,12 @@
 package dev.mayankg.design.patterns.creational.objectPool.example;
 
 class Client {
+    //Loading 5 objects of bitmap in the image pool with the same name
     private static final ObjectPool<Bitmap> imagePool =
             new ObjectPool<>(() -> new Bitmap("Logo.bmp"), 5);
 
     public static void main(String[] args) {
+        //Getting objects from objectPool and using them as regular objects
         Bitmap b1 = imagePool.get();
         b1.setLocation(new Point2D(10, 10));
         Bitmap b2 = imagePool.get();
@@ -13,6 +15,7 @@ class Client {
         b1.draw();
         b2.draw();
 
+        //Putting them back in the objectPool once their work is done!
         imagePool.release(b1);
         imagePool.release(b2);
     }
--- src/main/java/dev/mayankg/design/patterns/creational/objectPool/example/ObjectPool.java
+++ src/main/java/dev/mayankg/design/patterns/creational/objectPool/example/ObjectPool.java
@@ -4,30 +4,36 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.function.Supplier;
 
+/**
+ * Object Pool, here we are pre-creating objects in the pool
+ */
 class ObjectPool<T extends Poolable> {
     private BlockingQueue<T> availablePool;
 
-    public ObjectPool(Supplier<T> creator, int count) {
+    public ObjectPool(Supplier<T> creator, int preCache) {
         availablePool = new LinkedBlockingDeque<>();
-        for (int i = 0; i < count; i++)
+        for (int i = 0; i < preCache; i++)
             availablePool.offer(creator.get());
     }
 
+    /*GET must decide what to do when the pool is empty: wait or create a new object and return it.*/
     public T get() {
         try {
             return availablePool.take();
         } catch (InterruptedException e) {
-            System.err.println("GET was interrupted!!");
+            // Here we are waiting if the pool is empty. This has a -ve server impact.
+            System.err.println("GET was interrupted!! Waiting on the pooled queue");
         }
         return null;
     }
 
+    /*RELEASE is putting the obj back in the pool after its no more needed to be reused again sometime.*/
     public void release(T object) {
         object.reset();
         try {
             availablePool.put(object);
         } catch (InterruptedException e) {
-            System.err.println("GET was interrupted!!");
+            System.err.println("PUT was interrupted!! Waiting on the pooled queue");
         }
     }
 }
