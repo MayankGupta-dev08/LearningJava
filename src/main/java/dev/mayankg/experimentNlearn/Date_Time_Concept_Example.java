4d1f46f9a4446519c6939ecd927db57000cf18af
Mayank Gupta <ce.mayank8@gmail.com>
2024-03-04 23:19:35 +0530

    ExperimentNLearn: #Date_Time_Concept_Example

--- src/main/java/dev/mayankg/experimentNlearn/Date_Time_Concept_Example.java
+++ src/main/java/dev/mayankg/experimentNlearn/Date_Time_Concept_Example.java
@@ -1,6 +1,8 @@
 package dev.mayankg.experimentNlearn;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 class Date_Time_Concept_Example {
     public static void main(String[] args) {
@@ -24,5 +26,21 @@ class Date_Time_Concept_Example {
 
 //        zdt.withFixedOffsetZone(ZoneOffset.UTC)
 //        ZonedDateTime.parse(header.getString("timestamp")).withZoneSameInstant(ZoneOffset.UTC)
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("MM/dd/YYYY");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(df.format(now));
+
+        DateTimeFormatter tf = DateTimeFormatter.ofPattern("HHmm");
+        System.out.println(tf.format(now));
+        System.out.println(1000 + Integer.parseInt(tf.format(now)));
+
+        temp();
+    }
+
+    private static void temp() {
+        String[] abc = new String[]{"one", "two", "three"};
+        String ans = Arrays.stream(abc).reduce("", (a, b) -> a +","+ b).substring(1);
+        System.out.println(ans);
     }
 }
