cbfe3bd7b684e82f58355c6aef7eed465b39fbac
Mayank Gupta <ce.mayank8@gmail.com>
2024-04-01 21:09:02 +0530

    Data Structures: Binary Tree

--- src/main/java/dev/mayankg/dataStructures/binaryTree/BTBasicQuestions.java
+++ src/main/java/dev/mayankg/dataStructures/binaryTree/BTBasicQuestions.java
@@ -1,22 +1,22 @@
 package dev.mayankg.dataStructures.binaryTree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class BTBasicQuestions {
     public static void main(String[] args) {
         int[] preorderArr = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
-
         // creating a binary tree
         BinaryTree bt1 = buildTreeUsingPreorder(preorderArr);
-
-        //Checking the preorder traversal of the BT formed.
         printPreOrderTraversalOfBT(bt1);
-
-        //Checking the inorder traversal of the BT formed.
         printInOrderTraversalOfBT(bt1);
-
-        //Checking the postorder traversal of the BT formed.
         printPostOrderTraversalOfBT(bt1);
+        printLevelOrderTraversalOfBT(bt1);
+        printNumberOfNodesInBT(bt1);
+        printSumOfNodesInBT(bt1);
+        printDepthOfBT(bt1);
+        printDiameterOfBT(bt1);
     }
 
     /**
@@ -53,6 +53,43 @@ class BTBasicQuestions {
         ArrayList postOrderAns = new ArrayList();
         binaryTree.postOrderTraversal(binaryTree.getRoot(), postOrderAns);
         System.out.println("PostOrder Traversal: " + postOrderAns);
+    }
+
+    /**
+     * Question 5: Print levelorder traversal for a binary tree
+     */
+    static void printLevelOrderTraversalOfBT(BinaryTree binaryTree) {
+        ArrayList levelOrderAns = new ArrayList();
+        binaryTree.levelOrderTraversal(binaryTree.getRoot(), levelOrderAns);
+        System.out.println("LevelOrder Traversal: " + levelOrderAns);
+    }
+
+    /**
+     * Question 6: Print the number of the nodes in the binary tree
+     */
+    static void printNumberOfNodesInBT(BinaryTree binaryTree) {
+        System.out.println("Number of Nodes in the BT: " + binaryTree.countNodes(binaryTree.getRoot()));
+    }
+
+    /**
+     * Question 7: Print the sum of the nodes in the binary tree
+     */
+    static void printSumOfNodesInBT(BinaryTree binaryTree) {
+        System.out.println("Sum of all the nodes in the BT: " + binaryTree.sumOfNodes(binaryTree.getRoot()));
+    }
+
+    /**
+     * Question 8: Print the depth of the binary tree
+     */
+    static void printDepthOfBT(BinaryTree binaryTree) {
+        System.out.println("Depth of the BT: " + binaryTree.depthOfBT(binaryTree.getRoot()));
+    }
+
+    /**
+     * Question 9: Print the diameter of the binary tree
+     */
+    static void printDiameterOfBT(BinaryTree binaryTree) {
+        System.out.println("Diameter of the BT: " + binaryTree.diameterOfBT(binaryTree.getRoot()));
     }
 
     /**
@@ -125,6 +162,50 @@ class BTBasicQuestions {
             postOrderTraversal(root.left, ans);
             postOrderTraversal(root.right, ans);
             ans.add(root.data);
+        }
+
+        void levelOrderTraversal(Node root, ArrayList<Integer> ans) {
+            if (root == null)
+                return;
+
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(root);
+            queue.add(null);
+            while (!queue.isEmpty()) {
+                Node currNode = queue.poll();
+                if (currNode != null) {
+                    ans.add(currNode.data);
+                    if (currNode.left != null) queue.add(currNode.left);
+                    if (currNode.right != null) queue.add(currNode.right);
+                } else if (currNode == null && !queue.isEmpty()) {
+                    queue.add(null); //at level end
+                }
+            }
+        }
+
+        int countNodes(Node root) {
+            if (root == null) return 0;
+
+            return 1 + countNodes(root.left) + countNodes(root.right);
+        }
+
+        int sumOfNodes(Node root) {
+            if (root == null) return 0;
+
+            return root.data + sumOfNodes(root.left) + sumOfNodes(root.right);
+        }
+
+        int depthOfBT(Node root) {
+            if (root == null) return 0;
+
+            return 1 + Math.max(depthOfBT(root.left), depthOfBT(root.right));
+        }
+
+        int diameterOfBT(Node root) {
+            if (root == null) return 0;
+
+            int maxDiaOfSubTree = Math.max(diameterOfBT(root.left), diameterOfBT(root.right));
+            return Math.max(maxDiaOfSubTree, 1 + depthOfBT(root.left) + depthOfBT(root.right));
         }
     }
 }
