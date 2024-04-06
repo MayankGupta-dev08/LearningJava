c37d69ffc32b89dacd132d13b5b8825b32d2a52b
Mayank Gupta <ce.mayank8@gmail.com>
2024-04-06 17:55:54 +0530

    Data Structures: Stack

---
+++ src/main/java/dev/mayankg/dataStructures/stack/QPushAtBottomOfStack.java
@@ -0,0 +1,30 @@
+package dev.mayankg.dataStructures.stack;
+
+import java.util.Stack;
+
+public class QPushAtBottomOfStack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.add(1);
+        stack.add(2);
+        stack.add(3);
+
+        pushAtTheBottomOfStack(4, stack);
+        pushAtTheBottomOfStack(5, stack);
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+
+    static void pushAtTheBottomOfStack(int val, Stack<Integer> stack) {
+        if (stack.isEmpty()) {
+            stack.add(val);
+            return;
+        }
+
+        Integer top = stack.pop();
+        pushAtTheBottomOfStack(val, stack);
+        stack.add(top);
+    }
+}
---
+++ src/main/java/dev/mayankg/dataStructures/stack/QReverseAStack.java
@@ -0,0 +1,25 @@
+package dev.mayankg.dataStructures.stack;
+
+import java.util.Stack;
+
+public class QReverseAStack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.add(1);
+        stack.add(2);
+        stack.add(3);
+        stack.add(4);
+
+        System.out.println("Before reverse: " + stack);
+        reverseStack(stack);
+        System.out.println("After reverse : " + stack);
+    }
+
+    static void reverseStack(Stack<Integer> stack) {
+        if(stack.isEmpty()) return;
+
+        Integer top = stack.pop();
+        reverseStack(stack);
+        QPushAtBottomOfStack.pushAtTheBottomOfStack(top, stack);
+    }
+}
