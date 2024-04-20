2b54702d68fd7214752113b9214f077b6affecd9
Mayank Gupta <ce.mayank8@gmail.com>
2024-04-20 20:49:23 +0530

    Learning Java: Unit Testing #Mockito

--- src/test/java/dev/mayankg/unitTesting/mockito/gettingStarted/business/TodoBusinessImplMockTest.java
+++ src/test/java/dev/mayankg/unitTesting/mockito/gettingStarted/business/TodoBusinessImplMockTest.java
@@ -2,11 +2,14 @@ package dev.mayankg.unitTesting.mockito.gettingStarted.business;
 
 import dev.mayankg.unitTesting.mockito.gettingStarted.data.api.TodoService;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -62,7 +65,7 @@ class TodoBusinessImplMockTest {
     }
 
     @Test
-    void deleteTodosRelatedToSpringForMayank_usingMock() {
+    void deleteTodosRelatedToSpringForMayank_usingMockBDD() {
         /* setup */
         TodoService todoService = mock(TodoService.class);
         when(todoService.retrieveTodos("Mayank")).thenReturn(todos);
@@ -73,12 +76,28 @@ class TodoBusinessImplMockTest {
 
         /* assertion */
         verify(todoService).deleteTodos("Learn Dance"); //checking whether this call was actually made or not
-        /* the above thing could also be done in this way, this actually means when the actual method invocation happens
+        /* Similar could be done like line 81; this actually means when the actual method invocation happens
         then, for the mockService, it should not run deleteTodos for the passed argument */
         BDDMockito.then(todoService).should().deleteTodos("Learn Dance");
 
         verify(todoService, Mockito.never()).deleteTodos("Learn Spring MVC");
         verify(todoService, Mockito.never()).deleteTodos("Learn Spring");
         verify(todoService, Mockito.times(1)).deleteTodos("Learn Finance"); // atLeastOnce, atLeast
+    }
+
+    @Test
+    void deleteTodosRelatedToSpringForMayank_usingMockBDDArgumentCapture() {
+        /* setup */
+        ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+        TodoService todoService = mock(TodoService.class);
+        when(todoService.retrieveTodos("Mayank")).thenReturn(todos);
+        TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+
+        /* test method invocation */
+        todoBusinessImpl.deleteTodosRelatedToSpring("Mayank");
+
+        /* assertion */
+        BDDMockito.then(todoService).should().deleteTodos(stringArgumentCaptor.capture());
+        assertThat(stringArgumentCaptor.getValue(), is("Learn Dance"));
     }
 }
