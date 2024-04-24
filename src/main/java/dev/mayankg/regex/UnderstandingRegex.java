99252612f6ef835391c001d268b126bd3a4807b3
Mayank Gupta <ce.mayank8@gmail.com>
2024-04-25 02:18:17 +0530

    Regex: understanding regex

--- src/main/java/dev/mayankg/regex/UnderstandingRegex.java
+++ src/main/java/dev/mayankg/regex/UnderstandingRegex.java
@@ -7,11 +7,20 @@ import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
 /**
- * <a href="https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/util/regex/Pattern.html">Java Doc on Pattern</a>
+ * Regex Docs
+ *
+ * <p><a href="https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/util/regex/Pattern.html">Java Doc on Pattern</a>
  * <a href="https://docs.oracle.com/javase/tutorial/essential/regex/index.html">Java Tutorial on Regex</a>
+ *
+ * <p>Tool: <a href="https://regexr.com/">regexr website (online tool to learn, build, & test regex)</a>
  */
 @SuppressWarnings(value = {"unused"})
 class UnderstandingRegex {
+
+    private static final String alphaNumericRegex = "[a-zA-Z0-9]+";
+    private static final String phoneNumberRegex = "[0-9]{10}";
+    private static final String emailRegex = "^[a-zA-Z0-9_.$-]+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,3}$";
+
     public static void main(String[] args) {
         try (Scanner scn = new Scanner(System.in)) {
             while (true) {
