e550b488234523b7982ba6d7022b68e8f61c3a68
Mayank Gupta <ce.mayank8@gmail.com>
2024-04-25 01:44:56 +0530

    Regex: getting started...

--- src/main/java/dev/mayankg/regex/UnderstandingRegex.java
+++ src/main/java/dev/mayankg/regex/UnderstandingRegex.java
@@ -1,24 +1,50 @@
 package dev.mayankg.regex;
 
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 /**
  * <a href="https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/util/regex/Pattern.html">Java Doc on Pattern</a>
+ * <a href="https://docs.oracle.com/javase/tutorial/essential/regex/index.html">Java Tutorial on Regex</a>
  */
 @SuppressWarnings(value = {"unused"})
-public class UnderstandingRegex {
+class UnderstandingRegex {
     public static void main(String[] args) {
+        try (Scanner scn = new Scanner(System.in)) {
+            while (true) {
+                System.out.print("Enter the regular expression: ");
+                String regexFromUser = scn.nextLine();
 
+                System.out.print("Enter the string to be checked: ");
+                String stringFromUser = scn.nextLine();
+
+                System.out.printf("Result: %s%n", isMatches(regexFromUser, stringFromUser));
+
+                System.out.println("Do you want to exit?? Press 'y' for doing so!");
+                if (scn.nextLine().equalsIgnoreCase("y")) {
+                    System.out.println("~~~ Thanks, good bye!! ~~~");
+                    break;
+                }
+            }
+        } catch (PatternSyntaxException err) {
+            System.out.printf("Error Msg: %s%n", err.getMessage());
+            System.out.println("StackTrace: " + Arrays.toString(err.getStackTrace()));
+        }
     }
 
     private static void gettingStartedExample() {
-        String regex = "a*b";
-        Pattern pattern = Pattern.compile(regex);
-
+        String regex = "[a-zA-Z0-9]+";
         String input = "aaaaab";
-        Matcher matcher = pattern.matcher(input);
+        boolean matches = isMatches(regex, input);
+        System.out.println("result: " + matches);
+    }
 
-        System.out.println("result: " + matcher.matches());
+    private static boolean isMatches(String regex, String input) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
     }
 }
