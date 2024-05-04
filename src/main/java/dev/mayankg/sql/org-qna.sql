2bcf86fa4615f6c5b5bf4e9f3c751aba4d53d4f7
Mayank Gupta <ce.mayank8@gmail.com>
2024-05-04 08:36:15 +0530

    SQL : MSSQL # practice QnA

---
+++ src/main/java/dev/mayankg/sql/org-qna.sql
@@ -0,0 +1,153 @@
+# SQL QnAs
+
+-- Q-01. Write a SQL query to
+
+
+-- Q-02. Write a SQL query to
+
+
+-- Q-03. Write a SQL query to
+
+
+-- Q-04. Write a SQL query to
+
+
+-- Q-05. Write a SQL query to
+
+
+-- Q-06. Write a SQL query to
+
+
+-- Q-07. Write a SQL query to
+
+
+-- Q-08. Write a SQL query to
+
+
+-- Q-09. Write a SQL query to
+
+
+-- Q-10. Write a SQL query to
+
+
+-- Q-11. Write a SQL query to
+
+
+-- Q-12. Write a SQL query to
+
+
+-- Q-13. Write a SQL query to
+
+
+-- Q-14. Write a SQL query to
+
+
+-- Q-15. Write a SQL query to
+
+
+-- Q-16. Write a SQL query to
+
+
+-- Q-17. Write a SQL query to
+
+
+-- Q-18. Write a SQL query to
+
+
+-- Q-19. Write a SQL query to
+
+
+-- Q-20. Write a SQL query to
+
+
+-- Q-21. Write a SQL query to
+
+
+-- Q-22. Write a SQL query to
+
+
+-- Q-23. Write a SQL query to
+
+
+-- Q-24. Write a SQL query to
+
+
+-- Q-25. Write a SQL query to
+
+
+-- Q-26. Write a SQL query to
+
+
+-- Q-27. Write a SQL query to
+
+
+-- Q-28. Write a SQL query to
+
+
+-- Q-29. Write a SQL query to
+
+
+-- Q-30. Write a SQL query to
+
+
+-- Q-31. Write a SQL query to
+
+
+-- Q-32. Write a SQL query to
+
+
+-- Q-33. Write a SQL query to
+
+
+-- Q-34. Write a SQL query to
+
+
+-- Q-35. Write a SQL query to
+
+
+-- Q-36. Write a SQL query to
+
+
+-- Q-37. Write a SQL query to
+
+
+-- Q-38. Write a SQL query to
+
+
+-- Q-39. Write a SQL query to
+
+
+-- Q-40. Write a SQL query to
+
+
+-- Q-41. Write a SQL query to
+
+
+-- Q-42. Write a SQL query to
+
+
+-- Q-43. Write a SQL query to
+
+
+-- Q-44. Write a SQL query to
+
+
+-- Q-45. Write a SQL query to
+
+
+-- Q-46. Write a SQL query to
+
+
+-- Q-47. Write a SQL query to
+
+
+-- Q-48. Write a SQL query to
+
+
+-- Q-49. Write a SQL query to
+
+
+-- Q-50. Write a SQL query to
+
+
+-- Q-51. Write a SQL query to
