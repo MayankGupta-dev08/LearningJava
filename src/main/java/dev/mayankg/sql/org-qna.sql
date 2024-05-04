8d35c9c2b9f5dd205d61be2cef6d18dbf1d691d0
Mayank Gupta <ce.mayank8@gmail.com>
2024-05-04 14:45:39 +0530

    SQL : MSSQL # practice QnA

--- src/main/java/dev/mayankg/sql/org-qna.sql
+++ src/main/java/dev/mayankg/sql/org-qna.sql
@@ -1,153 +1,153 @@
 # SQL QnAs
 
--- Q-01. Write a SQL query to
+-- Q-01. Write a SQL query to fetch 'FIRST_NAME' from Worker table using the alias name as Worker_Name.
+SELECT first_name AS Worker_Name FROM Worker;
 
+-- Q-02. Write a SQL query to fetch 'FIRST_NAME' from the 'Worker' table in upper case.
 
--- Q-02. Write a SQL query to
 
+-- Q-03. Write a SQL query to fetch unique values of DEPARTMENT from the Worker table.
 
--- Q-03. Write a SQL query to
 
+-- Q-04. Write a SQL query to print the first three characters of the FIRST_NAME from the Worker table.
 
--- Q-04. Write a SQL query to
 
+-- Q-05. Write a SQL query to find the position of the alphabet ('b') in the FIRST_NAME column from the Worker table.
 
--- Q-05. Write a SQL query to
 
+-- Q-06. Write a SQL query to print the FIRSt_NAME from the Worker table after removing unique white spaces from the right side.
 
--- Q-06. Write a SQL query to
 
+-- Q-07. Write a SQL query to print the FIRSt_NAME from the Worker table after removing unique white spaces from the left side.
 
--- Q-07. Write a SQL query to
 
+-- Q-08. Write a SQL query to fetch the unique values of the DEPARTMENT from the Worker table and print its length.
 
--- Q-08. Write a SQL query to
 
+-- Q-09. Write a SQL query to print the FIRST_NAME from the Worker table after replacing 'a' with 'A'.
 
--- Q-09. Write a SQL query to
 
+-- Q-10. Write a SQL query to print the FIRST_NAME and LAST_NAME from the Worker table into a single column as FULL_NAME. 
 
--- Q-10. Write a SQL query to
 
+-- Q-11. Write a SQL query to print all Worker details from the Worker table and order them by FIRST_NAME in ascending.
 
--- Q-11. Write a SQL query to
 
+-- Q-12. Write a SQL query to print all the details for the Worker table and order them by FIRST_NAME in ascending and DEPARTMENT descending.
 
--- Q-12. Write a SQL query to
 
+-- Q-13. Write a SQL query to print details for the Workers with the FIRST_NAME as 'Vipul' and 'Satish' from the Worker table.
 
--- Q-13. Write a SQL query to
 
+-- Q-14. Write a SQL query to print details for the Workers with the FIRST_NAME not as 'Vipul' or 'Satish' from the Worker table.
 
--- Q-14. Write a SQL query to
 
+-- Q-15. Write a SQL query to print details of the Workers with DEPARTMENT as 'ADMIN' from the Worker table.
 
--- Q-15. Write a SQL query to
 
+-- Q-16. Write a SQL query to print the details of the workers whoese FIRST_NAME contains 'a'.
 
--- Q-16. Write a SQL query to
 
+-- Q-17. Write a SQL query to print the details of the workers whoese FIRST_NAME ends with 'a'.
 
--- Q-17. Write a SQL query to
 
+-- Q-18. Write a SQL query to print the details of the workers whoese FIRST_NAME ends with 'h' and contains six alphabets.
 
--- Q-18. Write a SQL query to
 
+-- Q-19. Write a SQL query to print details of the workers whose SALARY lies between 100000 and 500000.
 
--- Q-19. Write a SQL query to
 
+-- Q-20. Write a SQL query to print the details of the workers who joined in Feb'2014.
 
--- Q-20. Write a SQL query to
 
+-- Q-21. Write a SQL query to fetch the count of workers whose DEPARTMENT is 'Admin'.
 
--- Q-21. Write a SQL query to
 
+-- Q-22. Write a SQL query to fetch the worker's full name with salaries >= 50000 and salaries <= 100000.
 
--- Q-22. Write a SQL query to
 
+-- Q-23. Write a SQL query to fetch the number of workers in each department in descending order of the count.
 
--- Q-23. Write a SQL query to
 
+-- Q-24. Write a SQL query to print the details of workers who are also 'Managers'.
 
--- Q-24. Write a SQL query to
 
+-- Q-25. Write a SQL query to print the different titles in the ORG whose worker count > 1.
 
--- Q-25. Write a SQL query to
 
+-- Q-26. Write a SQL query to show only odd rows from the worker table.
 
--- Q-26. Write a SQL query to
 
+-- Q-27. Write a SQL query to show only even rows from the worker table.
 
--- Q-27. Write a SQL query to
 
+-- Q-28. Write a SQL query to clone a new table from worker table.
 
--- Q-28. Write a SQL query to
 
+-- Q-29. Write a SQL query to fetch the intersecting records from two tables.
 
--- Q-29. Write a SQL query to
 
+-- Q-30. Write a SQL query to show the records of one table that another table doesn't have.
 
--- Q-30. Write a SQL query to
 
+-- Q-31. Write a SQL query to show the current date and time.
 
--- Q-31. Write a SQL query to
 
+-- Q-32. Write a SQL query to show the top 5 records of a table and order by descending using 'SALARY'.
 
--- Q-32. Write a SQL query to
 
+-- Q-33. Write a SQL query to show the fifth highest salary from the Worker table.
 
--- Q-33. Write a SQL query to
 
+-- Q-34. Write a SQL query to show the fifth highest salary from the Worker table without using the LIMIT keyword.
 
--- Q-34. Write a SQL query to
 
+-- Q-35. Write a SQL query to fetch the list of workers with same salary from the Worker table.
 
--- Q-35. Write a SQL query to
 
+-- Q-36. Write a SQL query to show the second highest salary from the Worker table.
 
--- Q-36. Write a SQL query to
 
+-- Q-37. Write a SQL query to show the one row twice in the result from Worker table.
 
--- Q-37. Write a SQL query to
 
+-- Q-38. Write a SQL query to list WORKER_ID for workers who did not got the bonus.
 
--- Q-38. Write a SQL query to
 
+-- Q-39. Write a SQL query to fetch the first 50% records from the Worker table.
 
--- Q-39. Write a SQL query to
 
+-- Q-40. Write a SQL query to list all the DEPARTMENTS that have less than 1 worker in it.
 
--- Q-40. Write a SQL query to
 
+-- Q-41. Write a SQL query to list all the DEPARTMENTS and the number of workers in it.
 
--- Q-41. Write a SQL query to
 
+-- Q-42. Write a SQL query to show the last record from the Worker table.
 
--- Q-42. Write a SQL query to
 
+-- Q-43. Write a SQL query to fetch the first row of the Worker table.
 
--- Q-43. Write a SQL query to
 
+-- Q-44. Write a SQL query to fetch the last 5 records of the Worker table.
 
--- Q-44. Write a SQL query to
 
+-- Q-45. Write a SQL query to print the name of employees having the highest salary in each DEPARTMENT.
 
--- Q-45. Write a SQL query to
 
+-- Q-46. Write a SQL query to fetch three max salaries from a table using co-related subquery.
 
--- Q-46. Write a SQL query to
 
+-- Q-47. Write a SQL query to fetch three min salaries from a table using the co-related subquery.
 
--- Q-47. Write a SQL query to
 
+-- Q-48. Write a SQL query to fetch the nth max salaries from the Worker table.
 
--- Q-48. Write a SQL query to
 
+-- Q-49. Write a SQL query to fetch DEPARTMENTS along with the total salaries paid for each of them.
 
--- Q-49. Write a SQL query to
 
-
--- Q-50. Write a SQL query to
+-- Q-50. Write a SQL query to fetch the names of workers who earn the highest salaries.
 
 
 -- Q-51. Write a SQL query to
