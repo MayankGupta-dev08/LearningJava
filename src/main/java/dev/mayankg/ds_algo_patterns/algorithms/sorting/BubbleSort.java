ce01d906511b693bebae16601ac9bf468f58a8b5
Mayank Gupta <ce.mayank8@gmail.com>
2024-07-28 15:19:00 +0530

    DS-Algo-Patterns: Sorting #Bubble Sort Algo

--- src/main/java/dev/mayankg/ds_algo_patterns/algorithms/sorting/BubbleSort.java
+++ src/main/java/dev/mayankg/ds_algo_patterns/algorithms/sorting/BubbleSort.java
@@ -1,7 +1,5 @@
 package dev.mayankg.ds_algo_patterns.algorithms.sorting;
 
-import jdk.jfr.Description;
-
 /**
  * Bubble Sort aka Sinking Sort <br>
  * Time Complexity: O(n^2) <br>
@@ -10,17 +8,17 @@ import jdk.jfr.Description;
  * In-Place: Yes <br>
  */
 @SuppressWarnings({"unused"})
-@Description("Bubble Sort aka Sinking Sort")
-public class BubbleSort {
+public final class BubbleSort {
 
     /**
-     * Repeatedly swap the adjacent elements if they are in wrong order.
+     * Repeatedly swap the adjacent elements if they are in wrong order. <br>
+     * For asc order --> if a[j] > a[j+1] then swap a[j] and a[j+1]
      */
     public static <E extends Comparable<E>> void sort(E[] arr) {
         int N = arr.length;
         for (int i = 0; i < N - 1; i++) {
             for (int j = 0; j < N - 1 - i; j++) {
-                if (SortingUtil.less(arr[j + 1], arr[j]))
+                if (!SortingUtil.less(arr[j], arr[j + 1]))
                     SortingUtil.swap(arr, j, j + 1);
             }
         }
--- src/main/java/dev/mayankg/ds_algo_patterns/algorithms/sorting/SortingUtil.java
+++ src/main/java/dev/mayankg/ds_algo_patterns/algorithms/sorting/SortingUtil.java
@@ -1,20 +1,20 @@
 package dev.mayankg.ds_algo_patterns.algorithms.sorting;
 
 @SuppressWarnings("unused")
-public final class SortingUtil {
+final class SortingUtil {
 
     /**
      * Swaps the elements at the specified positions in the specified array.
      *
-     * @param T the class of the objects in the array
+     * @param T   the class of the objects in the array
      * @param arr the array in which to swap elements
      * @param i   the index of one element to be swapped
      * @param j   the index of the other element to be swapped
      */
     static <T> void swap(T[] arr, int i, int j) {
-        if (arr == null || i < 0 || j < 0 || i >= arr.length || j >= arr.length) {
+        if (arr == null || i < 0 || j < 0 || i >= arr.length || j >= arr.length)
             throw new IllegalArgumentException("Invalid index or null array");
-        }
+
         T temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
@@ -24,14 +24,14 @@ public final class SortingUtil {
      * Returns true if the first argument is less than the second argument.
      *
      * @param T the type of the arguments being compared
-     * @param a   the first object to be compared
-     * @param b   the second object to be compared
+     * @param a the first object to be compared
+     * @param b the second object to be compared
      * @return true if the first argument is less than the second argument, false otherwise
      */
     static <T extends Comparable<T>> boolean less(T a, T b) {
-        if (a == null || b == null) {
+        if (a == null || b == null)
             throw new IllegalArgumentException("Arguments must not be null");
-        }
+
         return a.compareTo(b) < 0;
     }
 }
