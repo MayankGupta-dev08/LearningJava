57445f701e03598964f4cf2d7539474f0d13e1a4
Mayank Gupta <ce.mayank8@gmail.com>
2024-10-27 15:29:56 +0530

    multithreading: future, future.cancel(boolean), future.isCancelled()

---
+++ src/main/java/dev/mayankg/multithreading/executorframework/example3/Main2.java
@@ -0,0 +1,45 @@
+package dev.mayankg.multithreading.executorframework.example3;
+
+import java.util.concurrent.*;
+
+class Main2 {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<Integer> future = executor.submit(() -> {
+            try {
+                System.out.println("Task 1: Sleeping for 2 seconds");
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted: " + e.getMessage());
+            }
+            return 42;
+        });
+
+        /*try {
+            System.out.println("Waiting for 1 second");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted: " + e.getMessage());
+        }*/
+
+        try {
+            System.out.println("Is future done: " + future.isDone());
+            System.out.println("Cancelling the future");
+            future.cancel(true);
+            System.out.println("Is future cancelled: " + future.isCancelled());
+            System.out.println("Is future done: " + future.isDone());
+            System.out.println("Future value: " + future.get());
+        } catch (InterruptedException | ExecutionException | CancellationException e) {
+            System.out.println("CancellationException occurred.");
+        }
+
+        executor.shutdown();
+        try {
+            executor.awaitTermination(1, java.util.concurrent.TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted: " + e.getMessage());
+        } finally {
+            System.out.println("All tasks are done!");
+        }
+    }
+}
