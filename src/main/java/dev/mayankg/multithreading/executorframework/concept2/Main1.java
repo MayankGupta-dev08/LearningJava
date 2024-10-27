b4cff8745f9e0e5ad8996f59e21167bca0198ef0
Mayank Gupta <ce.mayank8@gmail.com>
2024-10-27 15:46:32 +0530

    multithreading: concept2 # invokeAll, invokeAny, shutdown, awaitTermination

--- src/main/java/dev/mayankg/multithreading/executorframework/concept2/Main1.java
+++ src/main/java/dev/mayankg/multithreading/executorframework/concept2/Main1.java
@@ -1,23 +1,24 @@
-package dev.mayankg.multithreading.executorframework.example2;
+package dev.mayankg.multithreading.executorframework.concept2;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.*;
 
-class Main2 {
+class Main1 {
 
     /**
-     * Output: (will be random) <br>
-     * Callable2 <br>
-     * Callable3 <br>
+     * Output: <br>
      * Callable1 <br>
+     * Callable2 <br>
+     * 1 <br>
      * 2 <br>
+     * Task was cancelled: null <br>
      * All tasks are done! <br>
      * <p>
      * Process finished with exit code 0
      */
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(3);
+        ExecutorService executor = Executors.newFixedThreadPool(2);
 
         Callable<Integer> callable1 = generateIntCallable(1);
         Callable<Integer> callable2 = generateIntCallable(2);
@@ -25,12 +26,26 @@ class Main2 {
 
         List<Callable<Integer>> callables = Arrays.asList(callable1, callable2, callable3);
 
+        List<Future<Integer>> futures = null;
         try {
-            Integer i = executor.invokeAny(callables, 1, TimeUnit.SECONDS);
-            System.out.println(i);
+            futures = executor.invokeAll(callables, 1, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
-        } catch (ExecutionException e) {
-        } catch (TimeoutException e) {
+            System.out.println("Thread interrupted: " + e.getMessage());
+            Thread.currentThread().interrupt();
+        }
+
+
+        for (Future<Integer> future : futures) {
+            try {
+                System.out.println(future.get());
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted: " + e.getMessage());
+                Thread.currentThread().interrupt();
+            } catch (ExecutionException e) {
+                System.out.println("Execution exception: " + e.getMessage());
+            } catch (CancellationException e) {
+                System.out.println("Task was cancelled: " + e.getMessage());
+            }
         }
 
         executor.shutdown();
@@ -45,7 +60,7 @@ class Main2 {
 
     private static Callable<Integer> generateIntCallable(int i) {
         return () -> {
-            Thread.sleep(900);
+            Thread.sleep(990);
             System.out.println("Callable" + i);
             return i;
         };
--- src/main/java/dev/mayankg/multithreading/executorframework/concept2/Main2.java
+++ src/main/java/dev/mayankg/multithreading/executorframework/concept2/Main2.java
@@ -1,24 +1,23 @@
-package dev.mayankg.multithreading.executorframework.example2;
+package dev.mayankg.multithreading.executorframework.concept2;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.*;
 
-class Main1 {
+class Main2 {
 
     /**
-     * Output: <br>
-     * Callable1 <br>
+     * Output: (will be random) <br>
      * Callable2 <br>
-     * 1 <br>
+     * Callable3 <br>
+     * Callable1 <br>
      * 2 <br>
-     * Task was cancelled: null <br>
      * All tasks are done! <br>
      * <p>
      * Process finished with exit code 0
      */
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(2);
+        ExecutorService executor = Executors.newFixedThreadPool(3);
 
         Callable<Integer> callable1 = generateIntCallable(1);
         Callable<Integer> callable2 = generateIntCallable(2);
@@ -26,26 +25,12 @@ class Main1 {
 
         List<Callable<Integer>> callables = Arrays.asList(callable1, callable2, callable3);
 
-        List<Future<Integer>> futures = null;
         try {
-            futures = executor.invokeAll(callables, 1, TimeUnit.SECONDS);
+            Integer i = executor.invokeAny(callables, 1, TimeUnit.SECONDS);
+            System.out.println(i);
         } catch (InterruptedException e) {
-            System.out.println("Thread interrupted: " + e.getMessage());
-            Thread.currentThread().interrupt();
-        }
-
-
-        for (Future<Integer> future : futures) {
-            try {
-                System.out.println(future.get());
-            } catch (InterruptedException e) {
-                System.out.println("Thread interrupted: " + e.getMessage());
-                Thread.currentThread().interrupt();
-            } catch (ExecutionException e) {
-                System.out.println("Execution exception: " + e.getMessage());
-            } catch (CancellationException e) {
-                System.out.println("Task was cancelled: " + e.getMessage());
-            }
+        } catch (ExecutionException e) {
+        } catch (TimeoutException e) {
         }
 
         executor.shutdown();
@@ -60,7 +45,7 @@ class Main1 {
 
     private static Callable<Integer> generateIntCallable(int i) {
         return () -> {
-            Thread.sleep(990);
+            Thread.sleep(900);
             System.out.println("Callable" + i);
             return i;
         };
