b8b605ab2db97e4f5d05ac011097e51f69efd74d
Mayank Gupta <ce.mayank8@gmail.com>
2025-03-30 07:19:19 +0530

    sql challenge 1

--- src/main/java/dev/mayankg/db_concepts/mysql/shirts_db_challenge1.sql
+++ src/main/java/dev/mayankg/db_concepts/mysql/shirts_db_challenge1.sql
@@ -1,36 +1,36 @@
-# connecting to the mysql db - jdbc:mysql://localhost:3306/
+-- connecting to the mysql db - jdbc:mysql://localhost:3306/
 mysql -u root -p
-##or
+--or
 mysql -u root -pmy_password
 
-show databases;
-create database shirts_db;
-use shirts_db;
+SHOW DATABASES;
+CREATE DATABASE shirts_db;
+USE shirts_db;
 
-show tables;
-create table shirts(
-shirts_id int not null primary key auto_increment,
-article varchar(15) not null,
-color varchar(10) not null,
-shirt_size varchar(3) not null default 'm',
-last_worn int default 0
+SHOW TABLES;
+CREATE TABLE shirts (
+  shirts_id    INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  article      VARCHAR(15)  NOT NULL,
+  color        VARCHAR(10)  NOT NULL,
+  shirt_size   VARCHAR(3)   NOT NULL DEFAULT 'M',
+  last_worn    INT          DEFAULT 0
 );
-desc shirts;
+DESC shirts;
 
-insert into shirts (article, color, shirt_size, last_worn)
-values
-('t-shirt', 'white', 's', 10),
-('t-shirt', 'green', 's', 200),
-('polo shirt', 'black', 'm', 10),
-('tank top', 'blue', 's', 50),
-('t-shirt', 'pink', 's', 0),
-('polo shirt', 'red', 'm', 5),
-('tank top', 'white', 's', 200),
-('tank top', 'blue', 'm', 15);
+INSERT INTO shirts (article, color, shirt_size, last_worn)
+VALUES
+  ('t-shirt', 'white', 'S', 10),
+  ('t-shirt', 'green', 'S', 200),
+  ('polo shirt', 'black', 'M', 10),
+  ('tank top', 'blue', 'S', 50),
+  ('t-shirt', 'pink', 'S', 0),
+  ('polo shirt', 'red', 'M', 5),
+  ('tank top', 'white', 'S', 200),
+  ('tank top', 'blue', 'M', 15);
 
-select * from shirts;
+SELECT * FROM shirts;
 insert into shirts(article, color, shirt_size, last_worn) 
-	values('polo shirt', 'purple', 'm', 50);
+	values('polo shirt', 'purple', 'M', 50);
 select article,color,shirt_size,last_worn from shirts;
 
 update shirts set shirt_size='L' where article='polo shirt';
